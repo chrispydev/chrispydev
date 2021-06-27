1d50bc0dc0a047c411b2684f20c6549a6c502081
chrispydev <christianowusu44@gmail.com>
2021-06-27 20:39:45 +0000

    api fix

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,26 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 import BlogCard from '../components/BlogCard';
 import Footer from '../components/Footer';
 import { server } from '../config';
 
-function blog({ blog }) {
-  // const [blog, setBlog] = useState();
+function blog() {
+  const [blog, setBlog] = useState();
 
-  // useEffect(async () => {
-  //   const res = await fetch(`${server}/api/blog`, {
-  //     method: 'GET',
-  //     headers: {
-  //       // update with your user-agent
-  //       'User-Agent':
-  //         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-  //       Accept: 'application/json; charset=UTF-8',
-  //     },
-  //   });
-  //   const blog = await res.json();
-  //   setBlog(blog);
-  // }, []);
+  useEffect(async () => {
+    const res = await fetch(`${server}/api/blog`, {
+      method: 'GET',
+      headers: {
+        // update with your user-agent
+        'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        Accept: 'application/json; charset=UTF-8',
+      },
+    });
+    const blog = await res.json();
+    setBlog(blog);
+  }, []);
   return (
     <>
       {blog?.map(({ pic, title, detail, link }, index) => (
@@ -47,19 +47,19 @@ const BlogWrap = styled.section`
 
 export default blog;
 
-export async function getStaticProps() {
-  const res = await fetch(`${server}/api/blog`, {
-    method: 'GET',
-    headers: {
-      // update with your user-agent
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  });
-  const blog = await res.json();
+// export async function getStaticProps() {
+//   const res = await fetch(`${server}/api/blog`, {
+//     method: 'GET',
+//     headers: {
+//       // update with your user-agent
+//       'User-Agent':
+//         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+//       Accept: 'application/json; charset=UTF-8',
+//     },
+//   });
+//   const blog = await res.json();
 
-  return {
-    props: { blog },
-  };
-}
+//   return {
+//     props: { blog },
+//   };
+// }
