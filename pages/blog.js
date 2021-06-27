f836dc196cdbc16cbe787e11f2856bda0cd4ef47
chrispydev <christianowusu44@gmail.com>
2021-06-27 20:09:45 +0000

    json error

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,27 @@
 // import { useState, useEffect } from 'react';
 
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import BlogCard from '../components/BlogCard';
 import Footer from '../components/Footer';
 import { server } from '../config';
 
-function blog({ blog }) {
-  // const [blog, setBlog] = useState();
+function blog() {
+  const [blog, setBlog] = useState();
 
-  // useEffect(async () => {
-  //   const res = await fetch(`${server}/api/blog`, {
-  //     method: 'GET',
-  //     headers: {
-  //       // update with your user-agent
-  //       'User-Agent':
-  //         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-  //       Accept: 'application/json; charset=UTF-8',
-  //     },
-  //   });
-  //   const blog = await res.json();
-  //   setBlog(blog);
-  // }, []);
+  useEffect(async () => {
+    const res = await fetch(`${server}/api/blog`, {
+      method: 'GET',
+      headers: {
+        // update with your user-agent
+        'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        Accept: 'application/json; charset=UTF-8',
+      },
+    });
+    const blog = await res.json();
+    setBlog(blog);
+  }, []);
   return (
     <>
       {blog?.map(({ pic, title, detail, link }, index) => (
