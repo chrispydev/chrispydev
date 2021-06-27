02dc0c8a7977393991a1b1605f9cfde00e9d8129
chrispydev <christianowusu44@gmail.com>
2021-06-27 20:26:41 +0000

    blog api/blog

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,8 @@ import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
 
 import { server } from '../config';
-import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const [portfolios, setPortfolio] = useState();
-
-  useEffect(async () => {
-    const res = await fetch(`${server}/api/blog`, {
-      method: 'GET',
-      headers: {
-        // update with your user-agents
-        'User-Agent':
-          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-        Accept: 'application/json; charset=UTF-8',
-      },
-    });
-    const portfolio = await res.json();
-    setPortfolio(portfolio);
-  }, []);
+export default function Home({ portfolios }) {
   return (
     <>
       <Banner />
@@ -35,11 +19,11 @@ export default function Home() {
   );
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch(`${server}/api`);
-//   const portfolios = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(`${server}/api`);
+  const portfolios = await res.json();
 
-//   return {
-//     props: { portfolios },
-//   };
-// }
+  return {
+    props: { portfolios },
+  };
+}
