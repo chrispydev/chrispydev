35f63aab519a08f08132fde2847f20b67d4dd529
chrispydev <christianowusu44@gmail.com>
2021-06-27 20:13:31 +0000

    json error

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,24 @@ import Testimonials from '../components/Testimonials';
 import Footer from '../components/Footer';
 
 import { server } from '../config';
+import { useEffect, useState } from 'react';
 
 export default function Home({ portfolios }) {
+  const [portfolio, setPortfolio] = useState();
+
+  useEffect(async () => {
+    const res = await fetch(`${server}/api/blog`, {
+      method: 'GET',
+      headers: {
+        // update with your user-agent
+        'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+        Accept: 'application/json; charset=UTF-8',
+      },
+    });
+    const portfolio = await res.json();
+    setPortfolio(portfolio);
+  }, []);
   return (
     <>
       <Banner />
