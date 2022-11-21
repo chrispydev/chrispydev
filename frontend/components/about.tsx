d3e6dfcab09ffc95fcf1e29c9e2924a68700aabc
chrispydev <christianowusu44@gmail.com>
2022-11-21 14:52:51 +0000

    feat(file name): working on about section

--- frontend/components/about.tsx
+++ frontend/components/about.tsx
@@ -1,13 +1,29 @@
+import Image from 'next/image';
 import React from 'react';
+
+import AboutImage from '../public/about/about.png';
 
 const AboutSection: React.FC = () => {
   return (
-    <section className=''>
-      <article className='flex flex-col justify-center items-center max-w-[80%]'>
-        <div className='rounded-xl bg-plColor backdrop-blur-sm py-2 px-4 m-2'>
-          <p>Hello, I am a Full Stack Developer living in Ghana</p>
+    <section className='flex flex-col justify-center items-center px-5 lg:px-0 lg:max-w-[80%] mx-auto'>
+      <div className='rounded-xl text-xs md:text-base bg-plColor backdrop-blur-sm py-2 px-4 m-2'>
+        <p>Hello, I am a Full Stack Developer living in Ghana</p>
+      </div>
+      <div className='flex flex-col md:flex-row justify-between items-center mt-4'>
+        <div className='flex flex-col justify-start items-start'>
+          <h1 className='text-4xl font-bold'>Christian Owusu</h1>
+          <h4 className='text-xs lg:text-sm text-ellipsis overflow-hidden'>
+            (Man of God,Mobile dev, Web dev, Web scrapping)
+          </h4>
         </div>
-      </article>
+        <Image
+          src={AboutImage}
+          alt='About Image'
+          width={100}
+          height={100}
+          className='rounded-full border-white border-[4px]'
+        />
+      </div>
     </section>
   );
 };
