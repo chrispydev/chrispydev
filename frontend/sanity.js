5ebd88fe824c4daaf1bd7ac3621e072145dc80ed
chrispydev <christianowusu44@gmail.com>
2023-08-03 11:58:07 +0000

    refactor(file name): removed unused imports

--- frontend/sanity.js
+++ frontend/sanity.js
@@ -1,12 +1,13 @@
-import { createCurrentUserHook, createClient } from 'next-sanity';
-import createImageUrlBuilder from '@sanity/image-url';
+import { createClient } from "next-sanity";
+// import { createCurrentUserHook, createClient } from 'next-sanity';
+import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: '2021-03-25',
+  apiVersion: "2021-03-25",
 
-  useCdn: process.env.NODE_ENV === 'production',
+  useCdn: process.env.NODE_ENV === "production",
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 };
 
