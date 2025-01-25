82b87df1ccb5d1a52472c335f8007a8b20f71c24
chrispydev <christianowusu@gmail.com>
2025-01-25 21:41:11 +0000

    style(file name): added a link to the home page of takuya

--- frontend/components/footerText.tsx
+++ frontend/components/footerText.tsx
@@ -4,7 +4,7 @@ const FooterText: React.FC = () => {
   return (
     <article className='mt-4 text-gray-800 font-[400] text-sm'>
       &copy; {new Date().getFullYear()} Christian Owusu - contains code and
-      styles from Takuya Matsuyama...
+      styles from <a href="https://www.craftz.dog/">Takuya Matsuyama...</a>
     </article>
   );
 };
