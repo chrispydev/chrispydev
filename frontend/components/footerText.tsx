367527c603addbe481d99c2a71eb4f75bee6ef93
chrispydev <christianowusu@gmail.com>
2025-01-25 22:21:07 +0000

    feat(file name): footer acknowledgement link indicator

--- frontend/components/footerText.tsx
+++ frontend/components/footerText.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const FooterText: React.FC = () => {
   return (
-    <article className='mt-4 text-gray-800 font-[400] text-sm'>
+    <article className='my-4 text-gray-800 font-[400] text-sm'>
       &copy; {new Date().getFullYear()} Christian Owusu - contains code and
-      styles from <a href="https://www.craftz.dog/">Takuya Matsuyama...</a>
+      styles from <a className="text-dColor" href="https://www.craftz.dog/">Takuya Matsuyama...</a>
     </article>
   );
 };
