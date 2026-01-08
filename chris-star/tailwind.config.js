81332cc05d4cdef708b2fecdaf1b0cdc8827140a
chrispydev <christianowusu44@gmail.com>
2026-01-08 18:00:33 +0000

    perf(tailwindcss): aadd tailwind config for zed

---
+++ chris-star/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        'primary-light': 'var(--color-primary-light)',
+        secondary: 'var(--color-secondary)',
+        success: 'var(--color-success)',
+      },
+      fontFamily: {
+        sans: 'var(--font-sans)',
+      },
+    },
+  },
+  plugins: []
+}
---
+++ chris-star/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './data/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        'primary-light': 'var(--color-primary-light)',
+        secondary: 'var(--color-secondary)',
+        success: 'var(--color-success)',
+      },
+      fontFamily: {
+        sans: 'var(--font-sans)',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
